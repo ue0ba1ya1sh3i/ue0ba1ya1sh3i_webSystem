1555f56dc5f63ab6540d98e4f9d8e6a75694da9a
ue0ba1ya1hs3i <ue0ba1ya1sh3i@gmail.com>
2024-06-13 16:08:23 +0900

    html

--- index.js
+++ index.js
@@ -16,9 +16,37 @@ expressApp.set("view engine", "ejs");
 //静的ファイルの実装
 expressApp.use('/files', express.static('files'));
 
+//ページのデータ集
+const pages = {
+    home: [
+        `<div class="left"></div>
+        <div class="main">
+            <!--ここに要素が入る--> 
+        </div>
+        <div class="right"></div>`,
+
+        `main {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            display: flex;
+            transform: translate(-50%, -50%);
+            background-color: #f1f1f1e3;
+            padding: 10px;
+            width: 80%;
+            height: 70%;
+            border-radius: 30px;
+            z-index: 100;
+        }`
+    ]
+};
+
 //メインページの実装
 expressApp.get("/", (req, res) => {
-    res.render("index", {id: "home"});
+    res.render("index", {
+        id: "home",
+        html: pages.home[0],
+    });
 });
 
 //テストページ(HTML/CSS/JSの開発用)
